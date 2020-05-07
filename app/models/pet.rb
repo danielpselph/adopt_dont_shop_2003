8bf2a076937da7613c87bea42755729742eb3dfa
Daniel Selph <daniel.p.selph@gmail.com>
2020-05-07 11:21:49 -0600

    Remove required image from pets

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,5 +1,5 @@
 class Pet < ApplicationRecord
-    validates_presence_of :name, :image, :age, :sex
+    validates_presence_of :name, :age, :sex
     validates :adoption_status, inclusion: [true, false]
     belongs_to :shelter
 
--- spec/models/pet_spec.rb
+++ spec/models/pet_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 RSpec.describe Pet, type: :model do 
     describe "validations" do
         it { should validate_presence_of(:name) }
-        it { should validate_presence_of(:image) }
         it { should validate_presence_of(:sex) }
         it { should validate_presence_of(:age) }
     end
